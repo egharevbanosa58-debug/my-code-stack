37ce4764c8720a77cfc760227d8e5c7358880f24
Nosa <egharevbanosa58@gmail.com>
2026-03-29 00:20:52 -0900

    Added navigation options for both small screens and larger ones

--- my-stack/app/components/Nav.jsx
+++ my-stack/app/components/Nav.jsx
@@ -1,12 +1,15 @@
+"use client"
+
 import { motion } from "framer-motion";
-import { FiMenu, FiMoon } from "react-icons/fi";
+import { FiMenu, FiMoon, FiX } from "react-icons/fi";
 import Logo from "./Logo";
 import ThemeToggle from "./ThemeToggle";
 import HeroBtn from "./HeroBtn";
 import { useEffect, useState } from "react";
 
 export default function Nav() {
-    const [isScroll, setIsScroll] = useState(false)
+    const [isScroll, setIsScroll] = useState(false);
+    const [ isOpen, setIsOpen ] = useState(false);
 
     useEffect(() => {
         const handleScroll = () => {
@@ -42,6 +45,15 @@ export default function Nav() {
                 </a>
 
                 <div className="flex items-center gap-4">
+                    <div className="hidden md:flex gap-4 text-lg dark:text-slate-400 text-slate-600">
+                        {/* The Large screen navigation */}
+                        <a href="#first" className="hover:text-xl transition-all duration-400">Home</a>
+                        <a href="#skills" className="hover:text-xl transition-all duration-400">Skills</a>
+                        <a href="#project" className="hover:text-xl transition-all duration-400">Projects</a>
+                        <a href="#about" className="hover:text-xl transition-all duration-400">About</a>
+                        <a href="#contact" className="hover:text-xl transition-all duration-400">Contact</a>
+                    </div>
+
                     <div>
                         {/* The theme toggle button */}
                         <ThemeToggle />
@@ -61,10 +73,18 @@ export default function Nav() {
                         </a>
                     </div>
 
-                    <div className="">
+                    <div className="relative">
                         {/* The hamburger icon */}
 
-                        <FiMenu className="text-xl md:hidden" />
+                        {isOpen ? <button onClick={() => setIsOpen(!isOpen)}><FiX className="text-xl md:hidden" /></button> : <button onClick={() => setIsOpen(!isOpen)}><FiMenu className="text-xl md:hidden" /></button>}
+
+                        <div className={`${isOpen ? "flex" : "hidden"} flex flex-col gap-5 absolute p-5 -translate-x-60 translate-y-5 rounded-xl border-2 border-slate-400/20 bg-white dark:bg-neutral-900/80 w-64`}>
+                            <a href="#first" className="">Home</a>
+                            <a href="#skills" className="">Skills</a>
+                            <a href="#project" className="">Projects</a>
+                            <a href="#about" className="">About</a>
+                            <a href="#contact" className="">Contact</a>
+                        </div>
                     </div>
                 </div>
             </motion.nav>
