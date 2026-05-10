b24757e6bfc26ed20b843e77d97e9f2dec4ee697
Nosa <egharevbanosa58@gmail.com>
2026-05-10 10:37:40 -0900

    Added sitemap

---
+++ my-stack/app/sitemap.xml/route.js
@@ -0,0 +1,14 @@
+export async function GET() {
+  const xml = `<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+  <url>
+    <loc>https://nosa-stack.vercel.app/</loc>
+  </url>
+</urlset>`;
+
+  return new Response(xml, {
+    headers: {
+      "Content-Type": "application/xml",
+    },
+  });
+}
